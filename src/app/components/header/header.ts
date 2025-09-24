5898d7302f7d4bb4dda893cc8035e6c2f1875cfd
angie <angiegomez1616@gmail.com>
2025-09-24 17:55:02 -0500

    first commit

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import{ RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterModule],
+  templateUrl: './header.html',
+  styleUrls: ['./header.css']
+})
+export class Header {
+}
